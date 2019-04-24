6b67923016acad504ef82908971d2a35f7e5da77
Tyler Maddox <tyler.maddox@gmail.com>
2019-04-24 18:42:45 -0400

    Formatting

--- scraper.js
+++ scraper.js
@@ -1,9 +1,9 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
-const pcheerio = require('pseudo-cheerio');
+const pcheerio = require("pseudo-cheerio");
 
 const searchUrl = "https://www.imdb.com/find?&s=tt&ref_=fn_tt&q=";
-const movieUrl = "https://www.imdb.com/title/"
+const movieUrl = "https://www.imdb.com/title/";
 
 //
 
@@ -30,94 +30,100 @@ function searchMovies(searchTerm) {
 }
 
 function getMovie(imdbID) {
-    return fetch(`${movieUrl}${imdbID}`)
+  return fetch(`${movieUrl}${imdbID}`)
     .then(response => response.text())
     .then(body => {
-        const $ = cheerio.load(body);
-        const $title = $('.title_wrapper h1')
-        const title = $title.first().contents().filter(function() {
-            return this.type === 'text';
-        }).text().trim();
-        const rating = 	
-        $(".subtext")
-		    .clone()	
-		    .children()	
-		    .remove()	
-		    .end()	      
+      const $ = cheerio.load(body);
+      const $title = $(".title_wrapper h1");
+      const title = $title
+        .first()
+        .contents()
+        .filter(function() {
+          return this.type === "text";
+        })
         .text()
-        .replace(/,/g, '')
+        .trim();
+      const rating = $(".subtext")
+        .clone()
+        .children()
+        .remove()
+        .end()
+        .text()
+        .replace(/,/g, "")
         .trim();
 
-        const length = $('.subtext time').text().trim()
+      const length = $(".subtext time")
+        .text()
+        .trim();
 
-        const genres = new Array();
-        $('.subtext a').not('.subtext a[title]').each(function(i, elem) {
-            const genre = $(elem).text();
-            genres.push(genre);
-        })
+      const genres = new Array();
+      $(".subtext a")
+        .not(".subtext a[title]")
+        .each(function(i, elem) {
+          const genre = $(elem).text();
+          genres.push(genre);
+        });
 
-        const release = $('.subtext a[title]').text();
-        const score = $('span[itemprop="ratingValue"]').text();
-        const poster = $('div[class="poster"] a img').attr('src');
-        const summary = $('div.summary_text').text().trim();
+      const release = $(".subtext a[title]").text();
+      const score = $('span[itemprop="ratingValue"]').text();
+      const poster = $('div[class="poster"] a img').attr("src");
+      const summary = $("div.summary_text")
+        .text()
+        .trim();
 
-        const directors = new Array();
-        pcheerio.find($, "div.credit_summary_item:first a").each(function(i, elem) {
+      const directors = new Array();
+      pcheerio
+        .find($, "div.credit_summary_item:first a")
+        .each(function(i, elem) {
           const director = $(elem).text();
           directors.push(director);
         });
 
-        // const writers = new Array();
-        // pcheerio.find($, "div.plot_summary:nth-child(2) a").each(function(i, elem) {
-        //   const writer = $(elem).text();
-        //   writers.push(director);
-        // }); 
-        
-        const writers = new Array();
-        pcheerio.find($, ".credit_summary_item:eq(1) a").each(function(i, elem) {
-          const writer = $(elem).text();
-          writers.push(writer);
-          const lastIndex = writers[writers.length -1];
-          if(lastIndex.indexOf('more credits') !== -1) {
-            writers.pop();
-          }
-          return writers;
-        });
+      const writers = new Array();
+      pcheerio.find($, ".credit_summary_item:eq(1) a").each(function(i, elem) {
+        const writer = $(elem).text();
+        writers.push(writer);
+        const lastIndex = writers[writers.length - 1];
+        if (lastIndex.indexOf("more credits") !== -1) {
+          writers.pop();
+        }
+        return writers;
+      });
 
-        const stars = new Array();
-        pcheerio.find($, ".credit_summary_item:eq(2) a").each(function(i, elem) {
-          const star = $(elem).text();
-          stars.push(star);
-          const lastIndex = stars[stars.length -1];
-          if(lastIndex.indexOf('See full cast & crew') !== -1) {
-            stars.pop();
-          }
-          return stars;
-        });
+      const stars = new Array();
+      pcheerio.find($, ".credit_summary_item:eq(2) a").each(function(i, elem) {
+        const star = $(elem).text();
+        stars.push(star);
+        const lastIndex = stars[stars.length - 1];
+        if (lastIndex.indexOf("See full cast & crew") !== -1) {
+          stars.pop();
+        }
+        return stars;
+      });
 
-        const story = $('#titleStoryLine div p span').text().trim();
+      const story = $("#titleStoryLine div p span")
+        .text()
+        .trim();
 
-        return {
-          imdbID,
-          title,
-          rating,
-          length,
-          genres,
-          release,
-          score,
-          poster,
-          summary,
-          directors,
-          writers,
-          stars,
-          story
-        };
+      return {
+        imdbID,
+        title,
+        rating,
+        length,
+        genres,
+        release,
+        score,
+        poster,
+        summary,
+        directors,
+        writers,
+        stars,
+        story
+      };
     });
 }
 
 module.exports = {
-    searchMovies,
-    getMovie
+  searchMovies,
+  getMovie
 };
-
-// searchMovies("star wars");
